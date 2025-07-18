e15d20eac9c858f771df285ef1bb6dbb8d7f753a
Fahim Kamal Ahmed <fahimkamal63@gmail.com>
2025-07-18 22:11:13 +0600

    temp

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
+import './Header.css';
 
 const Header = ({ currentTheme, toggleTheme }) => {
   return (
     <div className="container">
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <Link to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <NavLink to="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
           Fahim Kamal Ahmed
-        </Link>
+        </NavLink>
 
-        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          <li><Link to="/" className="nav-link px-2 link-secondary">Home</Link></li>
-          <li><Link to="/projects" className="nav-link px-2 link-dark">Projects</Link></li>
-          <li><Link to="/contact" className="nav-link px-2 link-dark">Contact</Link></li>
+        <ul className="nav nav-masthead col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><NavLink to="/" className="nav-link px-2">Home</NavLink></li>
+          <li><NavLink to="/projects" className="nav-link px-2">Projects</NavLink></li>
+          <li><NavLink to="/contact" className="nav-link px-2">Contact</NavLink></li>
         </ul>
 
         <div className="col-md-3 text-end">
